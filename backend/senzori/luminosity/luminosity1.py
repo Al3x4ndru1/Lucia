dc0d07c70a44eeb4dc6f6ab62fdd7b7876ce0e6e
al3x4ndru1 <alexandruilie309@yahoo.com>
2022-03-24 12:01:30 +0000

    Commit

--- backend/senzori/luminosity/luminosity1.py
+++ backend/senzori/luminosity/luminosity1.py
@@ -1,13 +1,15 @@
 import time
 from random import seed
 from random import randint
-from backend.senzori.leds.ladd import led
+from backend.senzori.leds.ledsw import ladd
 
 import eel
 
 
-def luminosity():
+def luminosity(check):
     while True:
         var = randint(0, 100)
         eel.updateLuminosity(var)
+        if var <check:
+            ladd()
         time.sleep(5)
