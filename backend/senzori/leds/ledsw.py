ba607ddad08ff44b6b0bca214c4cc959200580d4
al3x4ndru1 <alexandruilie309@yahoo.com>
2022-03-24 10:22:29 +0000

    Commit

--- backend/senzori/leds/ledsw.py
+++ backend/senzori/leds/ledsw.py
@@ -1,36 +1,12 @@
-import math
-import sys
+import RPi.GPIO as GPIO
 import time
-from grove.adc import ADC
 
 
-class GroveLightSensor:
-
-    def __init__(self, channel):
-        self.channel = channel
-        self.adc = ADC()
-
-    @property
-    def light(self):
-        value = self.adc.read(self.channel)
-        return value
-
-
-Grove = GroveLightSensor
-
-
-def main():
-    if len(sys.argv) < 2:
-        print('Usage: {} adc_channel'.format(sys.argv[0]))
-        sys.exit(1)
-
-    sensor = GroveLightSensor(int(sys.argv[1]))
-
-    print('Detecting light...')
-    while True:
-        print('Light value: {0}'.format(sensor.light))
-        time.sleep(1)
-
-
-if __name__ == '__main__':
-    main()
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+print("LED on")
+GPIO.output(18,GPIO.HIGH)
+time.sleep(1)
+print("LED off")
+GPIO.output(18,GPIO.LOW)
