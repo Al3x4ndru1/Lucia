89bc74a5fb5496b964b605bc42c86833d536be18
al3x4ndru1 <alexandruilie309@yahoo.com>
2022-03-25 12:48:25 +0000

    Commit

--- backend/printing.py
+++ backend/printing.py
@@ -10,7 +10,7 @@ def credings():
     pathf = (os.getcwd() + '/backend/Csv_files')
     filenames = glob.glob(pathf + "/*.csv")
 
-
+    map={}
 
     dfs = []
     for filename in filenames:
@@ -20,6 +20,20 @@ def credings():
             csv_reader = reader(read_obj, delimiter=" ")
             # Iterate over each row in the csv using reader object
             for line in csv_reader:
-                print(line)
+                # row variable is a list that represents a row in csv
+                if(line[0]=="Name:"):
+                    a=line[1]
+
+                if line[0] == "Cost:":
+                    b=line[1]
 
 
+                if a in map:
+                    c=map.pop(a)
+                    c=b+c
+                    map[a]=c
+                else:
+                    map[a]=b
+
+
+    print(map)
